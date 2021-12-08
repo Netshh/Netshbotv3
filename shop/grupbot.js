8ff30ae25476a0afc1c9c8565c4f10cf7eaf36b5
Netshh <93586570+Netshh@users.noreply.github.com>
2021-12-08 16:26:05 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -3,7 +3,7 @@ const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
 1. *『NETSH BOT』Group*
-_https://chat.whatsapp.com/IKfk278D61h06fgJDkd2n6_
+_https://chat.whatsapp.com/JJk3EINE8BxE4tWQfIpQNq_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
